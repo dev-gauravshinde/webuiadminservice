fc68317293a7bc55849adfdc0fe0bac59b7abba6
Gaurav Shinde <gaurav.shinde@acplcargo.com>
2024-10-16 09:02:06 +0530

    menu master pagination table initial commit

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 const Index = lazy(() => import('../pages/Index'));
 const LoginBoxed = lazy(() => import('../pages/authentication/LoginBoxed'));
+const MenuMaster = lazy(() => import('../pages/masters/Menu'));
 
 const routes = [
     // dashboard
@@ -12,6 +13,11 @@ const routes = [
     {
         path: '/login',
         element: <LoginBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/menuMaster',
+        element: <MenuMaster />,
         layout: 'blank',
     },
 
